0037be826ffd768b09a48c009cc4adfe5a35a9df
huycuongz <HuyCuong@HP-CUONG-NOC>
2024-10-20 11:12:25 +0700

    Push session 2

---
+++ session2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// int add = 5+10;
+// Console.WriteLine($"Kết quả là {add}");
+
+// double kqchia = 5/2;
+// Console.WriteLine($"Kết quả chia {kqchia}");
+
+string number = Console.ReadLine();
+
+int convertNumber = Convert.ToInt32(number);
+
+Console.WriteLine($"ket qua {convertNumber}");
